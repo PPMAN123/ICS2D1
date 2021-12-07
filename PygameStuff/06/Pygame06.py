ada83f957b11b3dcf98395bede0ff9530fa60b5a
Ethan on Linux at home <ethanzhou76@gmail.com>
2021-12-07 18:29:09 -0500

    Finished Pygame 06

--- PygameStuff/06/Pygame06.py
+++ PygameStuff/06/Pygame06.py
@@ -8,24 +8,36 @@ pygame. init()
 
 size = (640, 480)
 screen = pygame.display.set_mode(size)
-pygame.display.set_caption("Pygame Drawing")
+background = pygame.Surface(size)
+background.fill("#87ceeb")
+screen.blit(background, (0,0))
+pygame.display.set_caption("Pink Church")
 
 #Polygons
 #pygame.draw.polygon(screen, colour, all the points)
 #Pay special attention to the number of brackets!
-pygame.draw.polygon(screen, (0,0,100), ((320, 0), (640,240), (640, 480), (0,480), (0, 240)))
+pygame.draw.polygon(screen, "#cb4154", ((320, 50), (640,240), (640, 480), (0,480), (0, 240)))
 
 #Rectangles
 #pygame.draw.rect(screen, colour, (x, y, width, height))
-pygame.draw.rect(screen, (255,255,255), (270, 280, 100, 200))
+pygame.draw.rect(screen, "#2e241b", (270, 280, 100, 200))
 
-#Circlesw
+#Circles
 #pygame.draw.circle(screen, colour, (x,y), radius)
-pygame.draw.circle(screen, (150,0,0), (320, 150), 50)
+pygame.draw.circle(screen, "#FFFFF7", (320, 150), 50)
+pygame.draw.circle(screen, (0,0,0), (360, 380), 10)
 
 #Lines
 #pygame.draw.line(screen, colour, (x1,y1), (x2, y2), linewidth)
-pygame.draw.line(screen, (20,150,20), (10,380), (500,300), 5)
+#window
+pygame.draw.line(screen, (0,0,0), (270,150), (370,150), 5)
+pygame.draw.line(screen, (0,0,0), (320,100), (320,200), 5)
+#door
+pygame.draw.line(screen, (0,0,0), (270,380), (350,380), 5)
+pygame.draw.line(screen, (0,0,0), (320,280), (320,480), 5)
+#cross
+pygame.draw.line(screen, "#cb4154", (320,0), (320,51), 5)
+pygame.draw.line(screen, "#cb4154", (300,25), (340,25), 5)
 
 
 pygame.display.flip()
