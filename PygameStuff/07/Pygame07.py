45788bcebac1d3accb8d0583274811e29c6c1605
Ethan on Linux at home <ethanzhou76@gmail.com>
2021-12-09 18:27:46 -0500

    Completed pygame 07

--- PygameStuff/07/Pygame07.py
+++ PygameStuff/07/Pygame07.py
@@ -3,6 +3,7 @@ Basic animation/movement of an image Surface
 """
 import pygame
 from pygame.locals import * 
+import random
 pygame.init()
 
 screen = pygame.display.set_mode((640, 480))
@@ -18,20 +19,30 @@ clock = pygame.time.Clock()
 keep_going = True
 moveBackTopLeft = False
 moveBackTopRight = False
+moveBackLeft = False
 timesLoaded = 0
+leftRightCounter = 0
+
 
 while keep_going:
     clock.tick(30)
     for ev in pygame.event.get():
         if ev.type == QUIT:
             keep_going = False
-    if timesLoaded == 1:
-        if img_rect.topright[0] > 640 or img_rect.topright[1] < 0:
-            moveBackTopRight = False
-            img_rect.top = 0
-            img_rect.left = 0
-            timesLoaded += 1
-            print(img_rect.topleft)
+
+    if timesLoaded == 2:
+        if not moveBackLeft and img_rect.topleft[0] == 0:
+            leftRightCounter += 1
+        if img_rect.topleft[0] < 640 and not moveBackLeft:
+            img_rect.left += 1
+        if img_rect.topright[0] > 640:
+            moveBackLeft = True
+            leftRightCounter += 1
+        if moveBackLeft:
+            img_rect.left -= 1
+        if img_rect.topleft[0] <= 0 and leftRightCounter == 2:
+            keep_going = False
+    elif timesLoaded == 1:
         if img_rect.bottomleft[0] > 0 and not moveBackTopRight:
             img_rect.top += 1
             img_rect.left -= 1
@@ -39,13 +50,17 @@ while keep_going:
             img_rect.top -= 1
             img_rect.left += 1
             moveBackTopRight = True
+        if img_rect.left > 140 or img_rect.top < 0:
+            moveBackTopRight = False
+            img_rect.top = random.randint(0,480 - img.get_height())
+            img_rect.left = 0
+            timesLoaded += 1
     else:
         if img_rect.topleft[0] < 0 or img_rect.topleft[1] < 0:
             moveBackTopLeft = False
             img_rect.top = 0
             img_rect.left = 140
             timesLoaded += 1
-            print(img_rect.topright)
         if img_rect.bottomright[0] < 640 and not moveBackTopLeft:
             img_rect.top += 1
             img_rect.left += 1
