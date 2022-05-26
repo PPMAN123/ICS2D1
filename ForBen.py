9a88be3820f36e6473ce71c104de3cbc97b3f01d
EthanOnXPS <ethanzhou76@gmail.com>
2022-05-26 10:13:29 -0400

    Ben for u again

--- ForBen.py
+++ ForBen.py
@@ -5,3 +5,11 @@ def draw_rectangle(width, height):
   print(width*'*')
 
 draw_rectangle(10,5)
+
+def draw_triangle(height):
+  num_of_stars = 1
+  for i in range(1, height+1):
+    print(' '*(height-i) + '*'*num_of_stars)
+    num_of_stars += 2
+
+draw_triangle(8)
